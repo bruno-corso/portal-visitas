c18963fd117099697b2f6fcdb32b1651683a5556
btcorso <uniebcorso@fei.edu.br>
2023-08-17 15:35:33 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //Criando estrutura de mapeamento de renderização dos componentes
+    <BrowserRouter>
+      <Routes>
+        {/* O caminho inicial base é '/' */}
+        <Route path='/' >
+          <Route index element={'inicio'}/>
+          <Route path='consulta-historico' element={'aqui será consultado o histórico das visitas'}/>
+          <Route path='visitacao-hoje' element={'quem está na empresa hoje'}/>
+          <Route path='agendamento' element={'aqui vai fazer o agendamento de visitas futuras'}/>
+          <Route path='agenda' element={'aqui vai vizualizar visitas futuras'}/>
+          <Route path='visitante' element={'aqui consulta um visitante em específico'}/>
+        </Route>
+        <Route path='*' element={'bug'}/>
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
